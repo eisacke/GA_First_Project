14f3fa7b50d970d253da7eb16775c1f37aff8bf5
eisacke <emily@isacke.com>
2015-07-10 16:56:17 +0100

    Pseudocoding

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-alert("hello")
+// When page loads, the user needs to choose whether to input their own word, or get a generated word
+// If input own word, input option is available with submit button
+// Input saved in an array
+// Give option 1 or 2 players
+// If 2 players then ask for names/team names
+// Once all player info is taken, load main page
+// Load the word, in hangman form (_ _ / _ _ etc.)
+// Load the letters a-z (maybe as buttons?)
+// Once clicked, the a-z button checks against the letters in the array
+// If true, the letter becomes visable, and the button disappears(blacked out)
+// It is still player 1's go
+// If false, the 'hangman' part is drawn, and the button disappears(blacked out)
+// If false, it is player 2's go
+// The player can guess the word by typing in a box at any point - but only on their go
+// If correct - declare winner
+// If incorrect - next person's go
+// If total hangman is drawn - game over - restart
+// Player 1 or player 2 score taken (this might be complicated, with player 1/2 toggle)
+// Back to menu page
